e3a5a4126583a21eb6cf678d4a58a4b0394062d4
Victor Ximenes <vxco@cin.ufpe.br>
2020-08-10 18:40:54 -0300

    Upload Binary_Search

---
+++ Binary_Search.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+using namespace std;
+
+int Binary_Search(int arr[] , int x , int l , int r){
+    int m = (l + r)/2;
+    if( x < arr[m]){
+        return Binary_Search(arr , x , l , m - 1);
+    }
+    else if(x > arr[m]){
+        return Binary_Search(arr, x , m +1 , r);
+    }
+    else if(x==arr[m]){
+        return m;
+    }
+    else{
+        return -1;
+    }
+}
+int main(){
+    #test
+    int arr[] = {1 ,2 ,3 , 4 , 5};
+    int index = Binary_Search(arr , 1 , 0 , 4);
+    cout << index << endl;
+    
+
+    return 0;
+}
